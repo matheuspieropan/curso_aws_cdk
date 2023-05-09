76250154eb686396dd14cf0dcef50569baebf4a5
PIEROPAN <matheus.pieropan@viannasempre.com.br>
2023-05-09 20:42:09 -0300

    Criação da vpc, cluster, service etc..

--- src/main/java/com/myorg/CursoAwsCdkApp.java
+++ src/main/java/com/myorg/CursoAwsCdkApp.java
@@ -1,42 +1,18 @@
 package com.myorg;
 
 import software.amazon.awscdk.App;
-import software.amazon.awscdk.Environment;
-import software.amazon.awscdk.StackProps;
-
-import java.util.Arrays;
 
 public class CursoAwsCdkApp {
+
     public static void main(final String[] args) {
         App app = new App();
 
-        new CursoAwsCdkStack(app, "CursoAwsCdkStack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+        VpcStack vpc = new VpcStack(app, "Vpc");
+        ClusterStack cluster = new ClusterStack(app, "Cluster", vpc.getVpc());
+        Service01Stack service01 = new Service01Stack(app, "Service01", cluster.getCluster());
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
-                .env(Environment.builder()
-                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                        .region(System.getenv("CDK_DEFAULT_REGION"))
-                        .build())
-                */
-
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
-                        .build())
-                */
-
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                .build());
-
+        cluster.addDependency(vpc);
+        service01.addDependency(cluster);
         app.synth();
     }
 }
-
--- src/main/java/com/myorg/CursoAwsCdkStack.java
+++ src/main/java/com/myorg/CursoAwsCdkStack.java
@@ -3,8 +3,6 @@ package com.myorg;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
 
 public class CursoAwsCdkStack extends Stack {
     public CursoAwsCdkStack(final Construct scope, final String id) {
@@ -13,12 +11,5 @@ public class CursoAwsCdkStack extends Stack {
 
     public CursoAwsCdkStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
-
-        // The code that defines your stack goes here
-
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "CursoAwsCdkQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
     }
 }
