8cc59d1bc0a5396f83aa11587668bef7bc991aae
Matheus Pieropan <56203846+matheuspieropan@users.noreply.github.com>
2023-05-27 23:53:43 -0300

    Update RdsStack.java

--- src/main/java/com/myorg/RdsStack.java
+++ src/main/java/com/myorg/RdsStack.java
@@ -22,7 +22,7 @@ public class RdsStack extends Stack {
                 type("String").build();
 
         ISecurityGroup iSecurityGroup = SecurityGroup.fromSecurityGroupId(this, id, vpc.getVpcDefaultSecurityGroup());
-        iSecurityGroup.addEgressRule(Peer.anyIpv4(), Port.tcp(3306));
+        iSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3306));
 
         DatabaseInstance databaseInstance = DatabaseInstance.Builder
                 .create(this, "Rds01")
