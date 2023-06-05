2859eb130fa9034bbadb792362d9c34df2c22043
mamapisouza <Pi204326@>
2023-06-04 21:20:54 -0300

    Criação do service 2

--- src/main/java/com/myorg/CursoAwsCdkApp.java
+++ src/main/java/com/myorg/CursoAwsCdkApp.java
@@ -17,7 +17,11 @@ public class CursoAwsCdkApp {
 
         SnsStack snsStack = new SnsStack(app, "Sns");
         Service01Stack service01 = new Service01Stack(app, "Service01", cluster.getCluster(),snsStack.getTopic());
+        Service02Stack service02 = new Service02Stack(app, "Service02", cluster.getCluster());
+
         service01.addDependency(cluster);
+        service02.addDependency(cluster);
+
         service01.addDependency(rdsStack);
         service01.addDependency(snsStack);
         app.synth();
