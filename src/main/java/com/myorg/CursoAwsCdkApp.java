64e4a319084a07c0e36ead690c91c947345ce8bc
mamapisouza <Pi204326@>
2023-06-04 21:32:09 -0300

    Criação do service 2 + SQS

--- src/main/java/com/myorg/CursoAwsCdkApp.java
+++ src/main/java/com/myorg/CursoAwsCdkApp.java
@@ -17,13 +17,13 @@ public class CursoAwsCdkApp {
 
         SnsStack snsStack = new SnsStack(app, "Sns");
         Service01Stack service01 = new Service01Stack(app, "Service01", cluster.getCluster(),snsStack.getTopic());
-        Service02Stack service02 = new Service02Stack(app, "Service02", cluster.getCluster());
+        Service02Stack service02 = new Service02Stack(app, "Service02", cluster.getCluster(), snsStack.getTopic());
 
         service01.addDependency(cluster);
-        service02.addDependency(cluster);
-
         service01.addDependency(rdsStack);
         service01.addDependency(snsStack);
+        service02.addDependency(cluster);
+        service02.addDependency(snsStack);
         app.synth();
     }
 }
--- src/main/java/com/myorg/Service02Stack.java
+++ src/main/java/com/myorg/Service02Stack.java
@@ -9,7 +9,11 @@ import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
 import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+import software.amazon.awscdk.services.events.targets.SnsTopic;
 import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
+import software.amazon.awscdk.services.sqs.DeadLetterQueue;
+import software.amazon.awscdk.services.sqs.Queue;
 import software.constructs.Construct;
 
 import java.util.HashMap;
@@ -17,15 +21,36 @@ import java.util.Map;
 
 public class Service02Stack extends Stack {
 
-    public Service02Stack(final Construct scope, final String id, Cluster cluster) {
-        this(scope, id, null, cluster);
+    public Service02Stack(final Construct scope, final String id, Cluster cluster, SnsTopic topic) {
+        this(scope, id, null, cluster, topic);
     }
 
-    public Service02Stack(final Construct scope, final String id, final StackProps props, Cluster cluster) {
+    public Service02Stack(final Construct scope, final String id, final StackProps props, Cluster cluster, SnsTopic topic) {
         super(scope, id, props);
+
+        Queue producoEventsDlq = Queue.Builder.create(this, "ProducoEventsDlq")
+                .queueName("ProducoEventsDlq")
+                .build();
+
+        DeadLetterQueue deadLetterQueue = DeadLetterQueue.builder()
+                .queue(producoEventsDlq)
+                .maxReceiveCount(3)
+                .build();
+
+        Queue producoEvents = Queue.Builder.create(this, "producoEvents")
+                .queueName("producoEvents")
+                .deadLetterQueue(deadLetterQueue)
+                .build();
+
+        SqsSubscription sqsSubscription = SqsSubscription.Builder
+                .create(producoEvents)
+                .build();
+
+        topic.getTopic().addSubscription(sqsSubscription);
 
         Map<String, String> envVariables = new HashMap<>();
         envVariables.put("aws.region", "us-east-1");
+        envVariables.put("AWS_SQS_PRODUTO_QUEUE", producoEvents.getQueueName());
 
         ApplicationLoadBalancedFargateService service = ApplicationLoadBalancedFargateService.
                 Builder.create(this, "ALB-02").
